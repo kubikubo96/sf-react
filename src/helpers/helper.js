4f07b9d633381c2ade88cf6395a1d15a642cb631
tiennt <tiennt@elcom.com.vn>
2022-09-28 08:54:47 +0700

    update helper

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -212,3 +212,27 @@ export function findInArray(aryData, key = '', value = '') {
 export function formatUnix(unix, format = 'DD MM YYYY H:mm:ss"') {
   return moment.unix(unix).format(format)
 }
+
+/**
+ * Convert giờ phút giây từ giây
+ * 
+ * @param {*} second 
+ * @returns 
+ */
+export function convertHMS(second) {
+  const sec = parseInt(second, 10); // convert value to number if it's string
+  let hours = Math.floor(sec / 3600); // get hours
+  let minutes = Math.floor((sec - (hours * 3600)) / 60); // get minutes
+  let seconds = sec - (hours * 3600) - (minutes * 60); //  get seconds
+  // add 0 if value < 10; Example: 2 => 02
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+  return hours + ':' + minutes + ':' + seconds; // Return is HH : MM : SS
+}
